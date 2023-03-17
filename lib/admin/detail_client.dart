7f37e4d08808dd7a1472254864eb70630b6b93e7
Omar DIOP <odiop6170@gmail.com>
2023-03-17 19:14:43 +0000

    ajout detail client

--- lib/admin/detail_client.dart
+++ lib/admin/detail_client.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gestion_hotel/api/apiMethode.dart';
 import 'package:gestion_hotel/start/titleText.dart';
+
+import '../start/subtitleText.dart';
 class DetailClient extends StatelessWidget {
   final id;
    const DetailClient({Key? key, required this.id}) : super(key: key);
@@ -12,11 +14,55 @@ class DetailClient extends StatelessWidget {
       body:FutureBuilder(
           future: getClient(id),
           builder:(context,snapshot){
+            print(snapshot.data);
             if(snapshot.data==null){
               return CircularProgressIndicator();
             }
             else{
-              return Text(snapshot.data['Prenom']);
+              List detail=snapshot.data;
+
+              return ListView(
+                children:detail.map((data){
+                  return Container(
+                    margin:const EdgeInsets.all(10),
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            SubTitleText(text:"Prenom : ", color:Colors.black,size: 14),
+                            TitleText(text: data['Prenom'], color:Colors.black,size: 17)
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            SubTitleText(text:"Nom : ", color:Colors.black,size: 14),
+                            TitleText(text: data['Name'], color:Colors.black,size: 17)
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            SubTitleText(text:"Creer le : ", color:Colors.black,size: 14),
+                            TitleText(text: data['CreatedAt'], color:Colors.black,size: 17)
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            SubTitleText(text:"Reservation : ", color:Colors.black,size: 14),
+                           // TitleText(text: data['ReservationClients'], color:Colors.black,size: 17)
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            SubTitleText(text:"Facture : ", color:Colors.black,size: 14),
+                          //  TitleText(text: data['FacturationClients'], color:Colors.black,size: 17)
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              );
             }
           }
       ),
